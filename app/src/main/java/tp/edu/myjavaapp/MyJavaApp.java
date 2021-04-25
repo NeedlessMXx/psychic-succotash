d37b36b265f05339de86e6b44df49a7a73c9e0d4
Shaun <needlessmxx.sc@gmail.com>
2021-04-26 00:33:07 +0800

    v1.0
    Initial Proj.

---
+++ app/src/main/java/tp/edu/myjavaapp/MyJavaApp.java
@@ -0,0 +1,36 @@
+package tp.edu.myjavaapp;
+
+public class MyJavaApp
+{
+    public static void main(String[] args)
+    {
+        String name = "Mary";
+        int age = 5;
+        double height = 1.23;
+        char grade = 'A';
+        boolean child = true;
+
+        double lengthoftable = 1.6;
+        String flower = "Angelica";
+        double price = 1199.99;
+        int age1 = 17;
+        char grade1 = 'B';
+        boolean rain = true;
+
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + "m");
+        System.out.println("Grade: " + grade);
+        System.out.println("Child: " + child);
+        System.out.println(" ");
+        System.out.println("The length of this table is " + lengthoftable + "m" );
+        System.out.println("The name of this flower is called " + flower);
+        System.out.println("The price of an iPhone is $" + price);
+        System.out.println("THe age of a student is " + age1 + " years old");
+        System.out.println("The grade of Chemistry is '" + grade1 + "'");
+        System.out.println("It is " + rain + " that the rain has stopped");
+
+    }
+
+}
