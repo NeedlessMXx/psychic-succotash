a5872578a2ee822b8da94f90393bc7b69589f832
Shaun <needlessmxx.sc@gmail.com>
2021-05-15 23:48:40 +0800

    v1.5
    added Assessment3

---
+++ app/src/main/java/tp/edu/myjavaapp/Assessment3.java
@@ -0,0 +1,12 @@
+package tp.edu.myjavaapp;
+
+public class Assessment3 {
+    public static void main(String[] args) {
+        int [] Arr = {55,24,65,41,13,22,76,90};
+        for (int i = 0;i<Arr.length;i++){
+            if(Arr[i] >= 55){
+                System.out.println("Numba: " + Arr[i]);
+            }
+        }
+    }
+}
